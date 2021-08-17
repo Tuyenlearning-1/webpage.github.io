9d9f1c4d2eaac44e8848045712d65c6aeb93201e
Tuyenlearning-1 <6051071139@st.utc2.edu.vn>
2021-08-17 22:09:31 +0700

    first

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// hieu ung hover anh
+const tilt = $('.thumb').tilt();
+
+
+
+// let circle__cards = document.querySelector('.statistic__circle');
+
+// console.log(circle__cards.getBoundingClientRect);
+
+
+
+// window.addEventListener("scroll", function(){
+//     if(window.pageYOffset >= circle__cards.offsetTop) {
+//         console.log("ok");
+//         circle__cards.cssText = "background-color: red;"
+//     }
+
+// })
